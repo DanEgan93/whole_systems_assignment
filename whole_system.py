7d6dddfdfdd3c6ed600e6bc4a6a9747f4f2d13d9
Daniel Egan <dan_egan@live.co.uk>
2019-07-30 15:28:07 +0100

    Addition of Entrez.entrez and Entrez.esummary to collect PubMed IDs and titles of papers associated with the SUFU gene

--- whole_system.py
+++ whole_system.py
@@ -1,7 +1,28 @@
+'''
+	Whole Systems Assignment
+
+	Task 1- Write a script to obtain a list of papers including the SUFU gene
+    		from the PubMed.The output from the script must be a .txt file 
+    		containing the list of papers.
+
+'''
+
 from Bio import Entrez
 
 Entrez.email = "dan_egan@live.co.uk"
-handle = Entrez.efetch(db="nucleotide", id="AY851612", rettype="gb", retmode="text")
-print(handle.readline())
+
+# esearch returns a list of ids for a pubmed search
+handle = Entrez.esearch(db='pubmed', retmax= '100000', retmode='xml', term='SUFU')
+data = Entrez.read(handle)
+sufu_list = data['IdList']
+handle.close()
+
+sufu_list = ",".join(map(str,sufu_list))
+
+paper_list = []
+handle = Entrez.esummary(db='pubmed', id=sufu_list, retmode='xml')
+sufu_details = Entrez.read(handle)
+for sub_record in sufu_details:
+	paper_list.append(sub_record['Title'])
 handle.close()
 
