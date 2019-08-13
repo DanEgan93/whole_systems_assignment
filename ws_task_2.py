b502c6f5d852bb1738265ccec4e9e7c6a40936f0
Daniel Egan <dan_egan@live.co.uk>
2019-08-13 17:18:59 +0100

    listing entrez and string gene lists

--- ws_task_2.py
+++ ws_task_2.py
@@ -7,6 +7,7 @@
 '''
 
 from Bio import Entrez
+import csv
 
 Entrez.email = "dan_egan@live.co.uk"
 
@@ -26,7 +27,7 @@ sufu_list_query = ",".join(map(str,sufu_list))
 handle = Entrez.esummary(db='gene', id=sufu_list_query, retmode='xml')
 sufu_details = Entrez.read(handle)
 
-list_of_genes = []
+entrez_list_of_genes = []
 
 # Returns in Dict(Dict(list_of_dict(dict)))
 
@@ -34,12 +35,30 @@ for k,v in sufu_details.items():
 	for e,i in v.items():
 		if type(i) == list:
 			for g in i:
-				list_of_genes.append(g['Name'].encode('UTF-8'))
+				entrez_list_of_genes.append(g['Name'].encode('UTF-8'))
 
 #comparing list of genes from entrez and string
+string_list_of_genes = []
 
+with open('string_interactions.tsv', 'r') as file:
+	tsvin = csv.reader(file, delimiter='\t')
+	for row in tsvin:
+		if row[0] == '#node1':
+			continue
+		string_list_of_genes.append(row[0])
+		string_list_of_genes.append(row[1])
+
+string_list_of_genes = set(string_list_of_genes)
+entrez_list_of_genes = set(entrez_list_of_genes)
+
+# Compating lists of genes
+gene_list_overlap = []
+gene_list_non_overlap = []
+
+print(len(entrez_list_of_genes))
+print(len(string_list_of_genes))
 
 #Adding genes in list to text file
 with open('entrez_gene_list.txt', 'w') as file:
-	for gene in list_of_genes:
+	for gene in entrez_list_of_genes:
 		file.write(gene+'\n')
