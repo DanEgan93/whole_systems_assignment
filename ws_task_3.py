68825c17e7e1775285324ca8d9945648f1ad28c6
Daniel Egan <dan_egan@live.co.uk>
2019-08-15 10:00:22 +0100

    First commit of task 3

---
+++ ws_task_3.py
@@ -0,0 +1,13 @@
+'''
+	Whole Systems Assignment
+
+	Task 2- Using the list of genes from task 2 extract information from ClinVar 
+	and dbSNP. For each gene in your list, create a text file containing:
+		1) All the SNPs within those genes that are in CLinVar and dbSNP
+		2) The clinical significance of each of the SNPs 
+		3) The disease or phenotype with which each of the SNPs is associated
+
+	Note- The list of genes used for this excercise will be the genes extracted
+	from the STRING database. This decision was made as this will limit the number
+	or requests to the Entrez API.
+'''
