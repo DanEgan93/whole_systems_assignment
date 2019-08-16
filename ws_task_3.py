85ce7d454f9d4b9eaf1fdea7da4307337174022c
Daniel Egan <dan_egan@live.co.uk>
2019-08-16 13:05:15 +0100

    Addition of detailed comments to ws_task_3.py script

--- ws_task_3.py
+++ ws_task_3.py
@@ -1,7 +1,7 @@
 '''
 	Whole Systems Assignment
 
-	Task 2- Using the list of genes from task 2 extract information from ClinVar 
+	Task 3- Using the list of genes from task 2 extract information from ClinVar 
 	and dbSNP. For each gene in your list, create a text file containing:
 		1) All the SNPs within those genes that are in CLinVar AND dbSNP
 		2) The clinical significance of each of the SNPs 
@@ -14,14 +14,13 @@
 
 from Bio import Entrez
 import csv
-import re
 
 Entrez.email = 'dan_egan@live.co.uk'
 
-
 ''' Creates a list of genes from the string_interactions.tsv file downloaded
-String databse.
+STRING database.
 '''
+
 string_list_of_genes = []
 
 with open('string_interactions.tsv', 'r') as file:
@@ -34,15 +33,16 @@ with open('string_interactions.tsv', 'r') as file:
 string_list_of_genes = set(string_list_of_genes)
 
 
-'''
-iterate through list of genes
-	1) get ids related to search term (clinvar)
-	2) uses esummary to exract variant info
-	3) repeat for (dbsnp)
-	4) combine the lists
-'''
-	
+
 def get_variant_info(database_name, search_term, retmax):
+	'''
+	Iterate through list of STRING genes
+		1) Get ids from the ClinVar database using the esearch functions related
+		to search term '*gene_name*[gene]'
+		2) Uses esummary to exract variant info (see desciption above)
+		3) Add info to a dictionary- clinvar_variant_dict
+		4) Return clinvar_variant_dict
+	'''
 	
 	handle = Entrez.esearch(
 		db = database_name, 
@@ -62,15 +62,19 @@ def get_variant_info(database_name, search_term, retmax):
 	clinvar_variant_dict = {}
 	dbsnp_variant_dict = {}
 	var_to_remove_list = []
-	#put it into a dict
 
+
+	'''
+	Esummary function returns gene associated data in the 
+	dict_1(dict_2(list_of_dicts(dict_3(dict_4)))) format. The for loop below itterates
+	through this structure to extract the gene name for each gene returned from
+	the search term described above. 
+
+	'''
 	for dict_1_key, dict_1_value in gene_details.items():
-		for dict_2_key, dict_2_value in dict_1_value.items():
-			# dict 2 value is a list of dicts containing individual entries
-			if type(dict_2_value) == list:
-				# capture different variables from clinvar or dbsnp
-				for dict_3 in dict_2_value:
-					# dict3 contains mutiple dicts
+		for dict_2_key, list_of_dicts in dict_1_value.items():
+			if type(list_of_dicts) == list:
+				for dict_3 in list_of_dicts:
 					for dict_4 in dict_3['variation_set']:
 						if len(dict_4['variation_xrefs']) != 0:
 							for source in dict_4['variation_xrefs']:
@@ -92,39 +96,43 @@ def get_variant_info(database_name, search_term, retmax):
 					}}
 				)
 
-	#create list of variants to remove from the dictionary
+	'''Creates a list of variants to remove from the dictionary including non-
+	SNPs and entries that do not have an rs number associated with them i.e.
+	the are not present in the dbSNP database.
+	'''
 	for variant_key,info_value in clinvar_variant_dict.items():
 		if info_value['rs_number'] == 'rsNA' or 'NM_' not in variant_key:
 			var_to_remove_list.append(variant_key)
-	# remove variants from dictionary
 	for variant in var_to_remove_list:
 		clinvar_variant_dict.pop(variant)
 	return clinvar_variant_dict
 
-
+# Creates gene_dict to store all SNPs from each gene in gene list
 gene_dict = {}
 
-
+# Calls the get_variant_info for each gene in the STRING gene list
 for gene in string_list_of_genes:
 	term = gene + '[gene]'
 	clinvar_dict = get_variant_info('clinvar', term, '100000')
 	gene_dict.update({gene:clinvar_dict} )	
 
-# writing all to .txt file
+# Writing SNP details for each gene to an independent .txt files
 for gene, variant_value in gene_dict.items():
 	with open('{}_snp_list.txt'.format(gene), 'w') as file:
 		file.write("{}\t{}\t{}\t{}\n".format(
 			'Variant',
-			'Variant type',
-			'rs_number',
-			'Pathogenicity'
+			'Clinical significance',
+			'Phenotype',
+			'rsID',
+			'Variant type'
 			)
 		)
 		for variant_key,variant_details in variant_value.items():
 				file.write("{}\t{}\t{}\t{}\n".format(
 				variant_key,
-				variant_details['variant_type'],
+				variant_details['pathogenicity'],
+				variant_details['phenotype'],
 				variant_details['rs_number'],
-				variant_details['pathogenicity']
+				variant_details['variant_type']
 				)	
 			)
